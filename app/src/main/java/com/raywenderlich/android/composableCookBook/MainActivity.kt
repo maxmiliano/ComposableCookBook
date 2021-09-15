9646e64ff5ff4dd1aa5a41a79a3aeb7a725d4f04
maxmiliano <maxmiliano@gmail.com>
2021-09-15 01:54:49 -0300

    Hello, (composable) World!

--- app/src/main/java/com/raywenderlich/android/composableCookBook/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/android/composableCookBook/MainActivity.kt
@@ -2,10 +2,15 @@ package com.raywenderlich.android.composableCookBook
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.compose.*
+import androidx.compose.material.Text
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+//    setContentView(R.layout.activity_main)
+    setContent {
+      Text("Hello, World!")
+    }
   }
 }
