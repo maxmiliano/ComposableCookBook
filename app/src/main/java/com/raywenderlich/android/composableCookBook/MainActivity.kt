7dfdc3a7af60a5d1aa3302f2814d8da474615b09
maxmiliano <maxmiliano@gmail.com>
2021-09-16 09:56:57 -0300

    Composable functions.

--- app/src/main/java/com/raywenderlich/android/composableCookBook/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/android/composableCookBook/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.compose.*
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 //    setContentView(R.layout.activity_main)
     setContent {
-      Text("Hello, World!")
+      Greeting()
     }
   }
+
+  @Composable
+  @Preview
+  fun Greeting() {
+    Text("Hello, World!", style = TextStyle(color = Color.Green))
+  }
+
 }
