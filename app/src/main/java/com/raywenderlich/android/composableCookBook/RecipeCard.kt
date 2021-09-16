c838108ef2b08a50fcda50c87a81a72c6894ca1a
maxmiliano <maxmiliano@gmail.com>
2021-09-16 12:39:08 -0300

    Recipe Card

---
+++ app/src/main/java/com/raywenderlich/android/composableCookBook/RecipeCard.kt
@@ -0,0 +1,47 @@
+package com.raywenderlich.android.composableCookBook
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun RecipeCard(recipe: Recipe) {
+    
+    Surface(shape = RoundedCornerShape(8.dp), elevation = 8.dp) {
+
+        val painter = painterResource(recipe.imageResource)
+
+        Column(modifier = Modifier.fillMaxWidth()) {
+            Image(painter = painter,
+                contentDescription = recipe.title,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(144.dp))
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(recipe.title, style = MaterialTheme.typography.h4)
+                recipe.ingredients.forEach { ingredient ->
+                    Text("• $ingredient")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+@Preview
+fun DefaultRecipeCard() {
+    RecipeCard(recipe = defaultRecipes[0])
+}
